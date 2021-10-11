0993ad7f95ef1757541271485ae914ad147eca36
TylerMair <tyleramair@gmail.com>
2021-10-11 12:25:33 -0700

    created manager object using inputs from inquirer

--- index.js
+++ index.js
@@ -1,59 +1,42 @@
 const inquirer = require('inquirer')
 const fs = require('fs');
 
+const Engineer = require('./lib/Engineer');
+const Manager = require('./lib/Manager');
+const Intern = require('./lib/Intern');
+let theManager ="";
 
 //questions for each member
 const manageQuestions = () => {
     inquirer.prompt([
     {
-        type = 'input',
-        name = 'name',
-        message = 'Welcome hotshot, please enter your name',
+        type: 'input',
+        name:'name',
+        message: 'Welcome hotshot, please enter your name',
     },
     {
-        name = 'managerID',
-        type = 'input',
-        message = 'Please enter your id'
+        name: 'managerID',
+        type: 'input',
+        message: 'Please enter your id'
     },
     {
-        name = 'email',
-        type = 'input',
-        message = 'Enter your email'
+        name: 'email',
+        type: 'input',
+        message: 'Enter your email'
     },
     {
-        name = 'officeName',
-        type = 'input',
-        message = 'What is your Office Number',
-    },
-    {
-        name = 'addEmployee',
-        type = 'list',
-        message = 'which type of employee would you like to add',
-        choices = ['Engineer', 'Intern']
+        name: 'officeName',
+        type: 'input',
+        message: 'What is your Office Number',
     }
+    
 ])
 .then(function(data){
-    switch(data){
-        case 'Engineer':
-            inquirer.prompt([{
-                name: 'github',
-                type: 'input',
-                message: 'Please enter Engineer github'
-            }
-            ])
-    }
-    switch(data){
-        case 'Intern':
-            inquirer.prompt([{
-                name: 'school',
-                type: 'input',
-                message: 'Please enter Intern school'
-
-            }])
-    }
+    theManager = new Manager(data.name, data.managerID, data.email, data.officeName)
+    console.log('The Manager', theManager);
 })
 }
-const intern = [
+/*const intern = [
     {
         name = 'internName',
         type = 'input',
@@ -76,7 +59,7 @@ const intern = [
 
     }
 ]
-
+*/
 /*const employ =
 inquirer.prompt()[{
     name: 'addEmployee',
@@ -84,13 +67,38 @@ inquirer.prompt()[{
     message: 'Would you like to add an employee',
 }];
 
-
+*/
 //generateCards = () => {
 
-function addEmployee(){
-    employ;
+/*function addEmployee(){
+      inquirer.prompt([
+        {
+            name = 'addEmployee',
+            type = 'list',
+            message = 'which type of employee would you like to add',
+            choices = ['Engineer', 'Intern']
+        }
+      ])
+      switch(data){
+        case 'Engineer':
+            inquirer.prompt([{
+                name: 'github',
+                type: 'input',
+                message: 'Please enter Engineer github'
+            }
+            ])
+    }
+    switch(data){
+        case 'Intern':
+            inquirer.prompt([{
+                name: 'school',
+                type: 'input',
+                message: 'Please enter Intern school'
+
+            }])
+    }
 }
-addEmployee();
+addEmployee(); */
 //}
-//generateCards(); */
+//generateCards(); 
 manageQuestions();
